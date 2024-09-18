03fd183085579c5692e5b1a477661b269ae2b398
ruviRedner <r.y.r7966@gmail.com>
2024-09-18 17:58:37 +0300

    finishProjectFlithg

--- flights/app.ts
+++ flights/app.ts
@@ -2,18 +2,22 @@ interface Flight {
     date: string,
     from:string,
     to:string,
-    id:number
+    id:string,
 }
 interface Passenger {
+    id:number,
     createdAt:string,
     name:string,
     gender:string,
-    flight_id:number,
+    flight_id:string,
     agent:string 
 }
+const selectFl:HTMLSelectElement = document.querySelector(".selectFlight")!
+const divPasenger:HTMLDivElement = document.querySelector(".containerS")!
 
-const agentId: string ="8289757";
-const baseUrl:string = "https://66e98a6387e417609449dfc5.mockapi.io/api/"
+
+const agentId: string ="agent 1";
+const baseUrl:string = "https://66ead5fe55ad32cda47a9f39.mockapi.io/api/"
 
 const getAllFlights = async ():Promise<void> => {
     try{
@@ -22,7 +26,14 @@ const getAllFlights = async ():Promise<void> => {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
         const flights: Flight[] = await res.json();
-        console.log(flights);
+        // console.log(flights);
+        for (const flight of flights) {
+            const option: HTMLOptionElement = document.createElement("option");
+            option.value = flight.id.toString();
+            option.textContent = `${flight.from} → ${flight.to} (${flight.date})`;
+            selectFl.appendChild(option);
+            
+        }
         
         
     }catch(error){
@@ -31,21 +42,86 @@ const getAllFlights = async ():Promise<void> => {
 }
 const getALLpasengers = async (agentid:string): Promise<void> => {
     try{
-        const res:Response = await fetch(`${baseUrl}pasangers/?agent=${agentid}`);
+        const res:Response = await fetch(`${baseUrl}pasengers/?agent=${agentid}`);
         if(!res.ok){
             throw new Error(`HTTP error! status: ${res.status}`);
         }
         const pasengers: Passenger[] = await res.json();
-        console.log(pasengers);
-        
-
+        // console.log(pasengers);
+        for (const pasenger of pasengers) {
+             const pName:HTMLParagraphElement = document.createElement("p");
+             pName.textContent = `Name: ${pasenger.name}, Gender: ${pasenger.gender}, Flight: ${pasenger.flight_id}`;
+             divPasenger.appendChild(pName);
+             const editButton: HTMLButtonElement = document.createElement("button");
+             editButton.textContent = "✏️";
+             editButton.id = "editBTN";
+             editButton.addEventListener("click",()=>{
+                 const divEdit = document.createElement("div");
+                 const labelName: HTMLLabelElement = document.createElement("label");
+                 labelName.textContent = "Name:";
+                 const inputName: HTMLInputElement = document.createElement("input");
+                 inputName.id = "editInput";
+                 labelName.appendChild(inputName);
+                 divEdit.appendChild(labelName);
+                 const mainDiv: HTMLDivElement = document.createElement("div");
+                 const h3: HTMLHeadingElement = document.createElement("h3");
+                 h3.textContent = "gender:";
+                 mainDiv.appendChild(h3);
+                 const divMale: HTMLDivElement = document.createElement("div");
+                 const genderName: HTMLInputElement = document.createElement("input");
+                 genderName.type = "radio";
+                 genderName.name = "gender";
+                 genderName.value = "male";
+                 genderName.textContent = "Male";
+                 genderName.checked = true;
+                 const maleName: HTMLLabelElement = document.createElement("label");
+                 maleName.textContent = "Male";
+                 maleName.appendChild(genderName);
+                 divMale.appendChild(maleName);
+                 mainDiv.appendChild(divMale);
+                 const divFemale: HTMLDivElement = document.createElement("div");
+                 const femaleName: HTMLInputElement = document.createElement("input");
+                 femaleName.type = "radio";
+                 femaleName.name = "gender";
+                 femaleName.value = "female";
+                 femaleName.textContent = "Female";
+                 const femaleLabel: HTMLLabelElement = document.createElement("label");
+                 femaleLabel.textContent = "Female";
+                 femaleLabel.appendChild(femaleName);
+                 divFemale.appendChild(femaleLabel);
+                 mainDiv.appendChild(divFemale);
+                 divEdit.appendChild(mainDiv);
+                 const saveButton: HTMLButtonElement = document.createElement("button");
+                 saveButton.textContent = "💾️"
+                 divEdit.appendChild(saveButton);
+                 divPasenger.appendChild(divEdit);
+                 saveButton.addEventListener("click",()=>{
+                    const selectedGender:HTMLInputElement = document.querySelector('input[name="gender"]:checked')!
+                    editPasenger(pasenger.id,{
+                        name: inputName.value,
+                        gender: selectedGender.value
+                    });
+                   
+                 })
+             })
+                 
+             const deleteButton: HTMLButtonElement = document.createElement("button");
+             deleteButton.textContent = "🗑️";
+             deleteButton.id = "deleteBTN";
+             deleteButton.addEventListener("click",()=>{
+                deletePasenger(pasenger.id);
+                window.location.reload();
+             })
+             divPasenger.appendChild(editButton);
+             divPasenger.appendChild(deleteButton);   
+        }
     }catch(error){
         console.log("Error fetching pasengers:", error);
     }
 }
-const addNewPasenger = async (passenger: Passenger): Promise<void> => {
+const addNewPasenger = async (passenger:Partial<Passenger>): Promise<void> => {
     try{
-        const res:Response = await fetch(`${baseUrl}pasangers`, {
+        const res:Response = await fetch(`${baseUrl}pasengers`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -57,13 +133,14 @@ const addNewPasenger = async (passenger: Passenger): Promise<void> => {
         }
         const newPassenger: Passenger = await res.json();
         console.log("New pasenger added:", newPassenger);
+
         }catch(error){
            console.error("Error adding new pasenger:", error);
         }};
 
-const editPasenger = async (passengerId: number, updatedPassenger: Passenger): Promise<void> => {
+const editPasenger = async (passengerId: number, updatedPassenger: Partial<Passenger>): Promise<void> => {
     try{
-        const res:Response = await fetch(`${baseUrl}pasangers/${passengerId}`, {
+        const res:Response = await fetch(`${baseUrl}pasengers/${passengerId}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -80,7 +157,7 @@ const editPasenger = async (passengerId: number, updatedPassenger: Passenger): P
         
 const deletePasenger = async (passengerId: number): Promise<void> => {
     try{
-        const res:Response = await fetch(`${baseUrl}pasangers/${passengerId}`, {
+        const res:Response = await fetch(`${baseUrl}pasengers/${passengerId}`, {
             method: 'DELETE'
         });
         if(!res.ok){
@@ -90,6 +167,22 @@ const deletePasenger = async (passengerId: number): Promise<void> => {
         }catch(error){
             console.error("Error deleting pasenger:", error);
         }};
+
+const btnDel: HTMLButtonElement = document.querySelector("#deleteBTN")!;
+const btnAdd: HTMLButtonElement = document.querySelector(".btnSent")!;
+const inputName: HTMLInputElement = document.querySelector(".inpName")!;
+const radioName: HTMLInputElement = document.querySelector(".rb")!
+
+btnAdd.addEventListener("click", ()=>{
+    addNewPasenger({
+        createdAt: "07/19/2024",
+        name: inputName.value,
+        gender: radioName.value,
+        flight_id: selectFl.value,
+        agent: agentId
+    });
+})
+
         
 
 
@@ -102,20 +195,20 @@ const deletePasenger = async (passengerId: number): Promise<void> => {
 
 
 
-// getAllFlights();
+getAllFlights();
 // addNewPasenger({
 //     createdAt: new Date().toISOString(),
 //     name: "ruvi",
 //     gender: "male",
-//     flight_id: 1,
+//     flight_id: "1",
 //     agent: agentId
 // });
-// getALLpasengers(agentId);
-// editPasenger(15, {
+getALLpasengers(agentId);
+// editPasenger(14, {
 //     createdAt: new Date().toISOString(),
 //     name: "ruvi updated test",
 //     gender: "male",
-//     flight_id: 1,
+//     flight_id: "1",
 //     agent: agentId
 // });
 // deletePasenger(15);
--- flights/js/app.js
+++ flights/js/app.js
@@ -8,8 +8,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const agentId = "8289757";
-const baseUrl = "https://66e98a6387e417609449dfc5.mockapi.io/api/";
+const selectFl = document.querySelector(".selectFlight");
+const divPasenger = document.querySelector(".containerS");
+const agentId = "agent 1";
+const baseUrl = "https://66ead5fe55ad32cda47a9f39.mockapi.io/api/";
 const getAllFlights = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const res = yield fetch(`${baseUrl}flights`);
@@ -17,7 +19,13 @@ const getAllFlights = () => __awaiter(void 0, void 0, void 0, function* () {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
         const flights = yield res.json();
-        console.log(flights);
+        // console.log(flights);
+        for (const flight of flights) {
+            const option = document.createElement("option");
+            option.value = flight.id.toString();
+            option.textContent = `${flight.from} → ${flight.to} (${flight.date})`;
+            selectFl.appendChild(option);
+        }
     }
     catch (error) {
         console.error("Error fetching flights:", error);
@@ -25,12 +33,77 @@ const getAllFlights = () => __awaiter(void 0, void 0, void 0, function* () {
 });
 const getALLpasengers = (agentid) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const res = yield fetch(`${baseUrl}pasangers/?agent=${agentid}`);
+        const res = yield fetch(`${baseUrl}pasengers/?agent=${agentid}`);
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
         const pasengers = yield res.json();
-        console.log(pasengers);
+        // console.log(pasengers);
+        for (const pasenger of pasengers) {
+            const pName = document.createElement("p");
+            pName.textContent = `Name: ${pasenger.name}, Gender: ${pasenger.gender}, Flight: ${pasenger.flight_id}`;
+            divPasenger.appendChild(pName);
+            const editButton = document.createElement("button");
+            editButton.textContent = "✏️";
+            editButton.id = "editBTN";
+            editButton.addEventListener("click", () => {
+                const divEdit = document.createElement("div");
+                const labelName = document.createElement("label");
+                labelName.textContent = "Name:";
+                const inputName = document.createElement("input");
+                inputName.id = "editInput";
+                labelName.appendChild(inputName);
+                divEdit.appendChild(labelName);
+                const mainDiv = document.createElement("div");
+                const h3 = document.createElement("h3");
+                h3.textContent = "gender:";
+                mainDiv.appendChild(h3);
+                const divMale = document.createElement("div");
+                const genderName = document.createElement("input");
+                genderName.type = "radio";
+                genderName.name = "gender";
+                genderName.value = "male";
+                genderName.textContent = "Male";
+                genderName.checked = true;
+                const maleName = document.createElement("label");
+                maleName.textContent = "Male";
+                maleName.appendChild(genderName);
+                divMale.appendChild(maleName);
+                mainDiv.appendChild(divMale);
+                const divFemale = document.createElement("div");
+                const femaleName = document.createElement("input");
+                femaleName.type = "radio";
+                femaleName.name = "gender";
+                femaleName.value = "female";
+                femaleName.textContent = "Female";
+                const femaleLabel = document.createElement("label");
+                femaleLabel.textContent = "Female";
+                femaleLabel.appendChild(femaleName);
+                divFemale.appendChild(femaleLabel);
+                mainDiv.appendChild(divFemale);
+                divEdit.appendChild(mainDiv);
+                const saveButton = document.createElement("button");
+                saveButton.textContent = "💾️";
+                divEdit.appendChild(saveButton);
+                divPasenger.appendChild(divEdit);
+                saveButton.addEventListener("click", () => {
+                    const selectedGender = document.querySelector('input[name="gender"]:checked');
+                    editPasenger(pasenger.id, {
+                        name: inputName.value,
+                        gender: selectedGender.value
+                    });
+                });
+            });
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "🗑️";
+            deleteButton.id = "deleteBTN";
+            deleteButton.addEventListener("click", () => {
+                deletePasenger(pasenger.id);
+                window.location.reload();
+            });
+            divPasenger.appendChild(editButton);
+            divPasenger.appendChild(deleteButton);
+        }
     }
     catch (error) {
         console.log("Error fetching pasengers:", error);
@@ -38,7 +111,7 @@ const getALLpasengers = (agentid) => __awaiter(void 0, void 0, void 0, function*
 });
 const addNewPasenger = (passenger) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const res = yield fetch(`${baseUrl}pasangers`, {
+        const res = yield fetch(`${baseUrl}pasengers`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -57,7 +130,7 @@ const addNewPasenger = (passenger) => __awaiter(void 0, void 0, void 0, function
 });
 const editPasenger = (passengerId, updatedPassenger) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const res = yield fetch(`${baseUrl}pasangers/${passengerId}`, {
+        const res = yield fetch(`${baseUrl}pasengers/${passengerId}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -75,7 +148,7 @@ const editPasenger = (passengerId, updatedPassenger) => __awaiter(void 0, void 0
 });
 const deletePasenger = (passengerId) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const res = yield fetch(`${baseUrl}pasangers/${passengerId}`, {
+        const res = yield fetch(`${baseUrl}pasengers/${passengerId}`, {
             method: 'DELETE'
         });
         if (!res.ok) {
@@ -87,20 +160,33 @@ const deletePasenger = (passengerId) => __awaiter(void 0, void 0, void 0, functi
         console.error("Error deleting pasenger:", error);
     }
 });
-// getAllFlights();
+const btnDel = document.querySelector("#deleteBTN");
+const btnAdd = document.querySelector(".btnSent");
+const inputName = document.querySelector(".inpName");
+const radioName = document.querySelector(".rb");
+btnAdd.addEventListener("click", () => {
+    addNewPasenger({
+        createdAt: "07/19/2024",
+        name: inputName.value,
+        gender: radioName.value,
+        flight_id: selectFl.value,
+        agent: agentId
+    });
+});
+getAllFlights();
 // addNewPasenger({
 //     createdAt: new Date().toISOString(),
 //     name: "ruvi",
 //     gender: "male",
-//     flight_id: 1,
+//     flight_id: "1",
 //     agent: agentId
 // });
-// getALLpasengers(agentId);
-// editPasenger(15, {
+getALLpasengers(agentId);
+// editPasenger(14, {
 //     createdAt: new Date().toISOString(),
 //     name: "ruvi updated test",
 //     gender: "male",
-//     flight_id: 1,
+//     flight_id: "1",
 //     agent: agentId
 // });
 // deletePasenger(15);
